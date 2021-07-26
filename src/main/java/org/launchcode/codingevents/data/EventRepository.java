5d222f9f8aae38c3699e399adb79d5878b4b098f
mmulle002 <matthew_muller@cable.comcast.com>
2021-07-26 15:28:53 -0400

    done chp 17 videos

--- src/main/java/org/launchcode/codingevents/data/EventRepository.java
+++ src/main/java/org/launchcode/codingevents/data/EventRepository.java
@@ -7,6 +7,11 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by Chris Bay
  */
+
 @Repository
 public interface EventRepository extends CrudRepository<Event, Integer> {
+
 }
+
+
+
